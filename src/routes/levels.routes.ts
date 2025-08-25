263989629524c7d55be96fe3eca1b592267ea920
Priyanshu Tiwari <Priyanshutiwari878@yahoo.com>
2025-08-25 21:20:42 +0530

    commit migration

--- src/routes/levels.routes.ts
+++ src/routes/levels.routes.ts
@@ -4,7 +4,7 @@ import { LevelController } from "../controllers/levels.controller";
 const ctfLevelRouter = Router();
 const ctfLevel = new LevelController();
 ctfLevelRouter.route("/createLevel").post(ctfLevel.createLevel);
-ctfLevelRouter.route("/getctfLevel/:uniqueId").post(ctfLevel.getLevel);
+ctfLevelRouter.route("/getctfLevel/:uniqueId").get(ctfLevel.getLevel);
 ctfLevelRouter.route("/getAllctfLevel").post(ctfLevel.getAllLevels);
 ctfLevelRouter.route("/deleteCtfLevel/:uniqueId").post(ctfLevel.deleteLevel);
 
